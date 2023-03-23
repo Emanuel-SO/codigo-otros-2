7db75aaa8922caa8f5d935b0027433ee16f9c1d6
Emanuel Saucedo <emanuelsaucedo52@gmail.com>
2023-03-22 23:19:36 -0600

    revicion de codigo alguien mas 2

--- formulario.js
+++ formulario.js
@@ -1,44 +1,48 @@
-var formulario = document.querySelector("#form")
+//se trae el form por class
+var formulario = document.querySelector(".formulario");
 
+//segun el submit del formulario se ejecuta la funcion
 formulario.onsubmit = function(e) {
 
-  e.prevent();
+  //hace que no se refresque la página a pesar de ser submit
+  e.preventDefault();
   
+  //se obtienen los inputs del formulario y se guardan en variables
   var n = formulario.elements[0]
-  var e = formulario.elements[1]
+  var ed = formulario.elements[1]
   var na = formulario.elements[2]
 
+  //se asignan los valores de los inputs a varibles
   var nombre = n.value
-  var edad = e.value
-
+  var edad = ed.value
   var i = na.selectedIndex
   var nacionalidad = na.options[i].value
-  console.log(nombre, edad)
-  console.log(nacionalidad)
 
+  // console.log(nombre, edad)
+  // console.log(nacionalidad)
+//si el nombre no cumple con la condicion se agrega una clase al elemeto HTML
   if (nombre.length === 0) {
     n.classList.add("error")
   }
+//si la edad no cumple con la condicion se agrega una clase al elemeto HTML
   if (edad < 18 || edad > 120) {
-    e.classList.add("error")
+    ed.classList.add("error")
   }
-
+//si se cumplen las condiciones se ejecuta la funcion que agrega a los invitados -> se le pasan las varibles a la funcion
 if (nombre.length > 0 
   && (edad > 18 
     && edad < 120) ) {
-  agregarInvitado(nombre, edad, nacionalidad)
+  agregarInvitado(nombre, edad, nacionalidad);
   }
 }
 
-var botonBorrar = document.createElement("button")
-botonBorrar.textContent = "Eliminar invitado"
-botonBorrar.id = "boton-borrar"
-var corteLinea = document.createElement("br")
-document.body.appendChild(corteLinea)
-document.body.appendChild(botonBorrar);
+// agrega un break al HTML
+var corteLinea = document.createElement("br");
+document.body.appendChild(corteLinea);
 
+//funcion que recibe como parametros los valores del formulario para agregar nuevos invitados
 function agregarInvitado(nombre, edad, nacionalidad) {
-
+  //grupo de condicionales que cambian los valores del input nacionalidad a una cadena de caracteres
   if (nacionalidad === "ar") {
     nacionalidad = "Argentina"
   }
@@ -52,46 +56,44 @@ function agregarInvitado(nombre, edad, nacionalidad) {
     nacionalidad = "Peruana"
   }
 
-var lista = document.getElementById("lista-de-invitados")
+  //se seleciona un elemento de HTML donde se agregaran los invitados
+  var lista = document.getElementById("lista-de-invitados")
 
-var elementoLista = document.createElement("div")
-elementoLista.classList.added("elemento-lista")
-lista.appendChild(elementoLista)
-
-var spanNombre = document.createElement("span")
-var inputNombre = document.createElement("input")
-var espacio = document.createElement("br")
-spanNombre.textContent = "Nombre: "
-inputNombre.value = nombre 
-elementoLista.appendChild(spanNombre)
-elementoLista.appendChild(inputNombre)
-elementoLista.appendChild(espacio)
-
-function crearElemento(descripcion, valor) {
-var spanNombre = document.createElement("span")
-var inputNombre = document.createElement("input")
-var espacio = document.createElement("br")
-spanNombre.textContent = descripcion + ": "
-inputNombre.value = valor 
-elementoLista.appendChild(spanNombre)
-elementoLista.appendChild(inputNombre)
-elementoLista.appendChild(espacio)
-}
-
-crearElemento("Nombre", nombre)
-crearElemento("Edad", edad)
-crearElemento("Nacionalidad", nacionalidad)
+  //se crea un elemento HTML que alamcenera la informacion de los invitados
+  var elementoLista = document.createElement("div")
+  //se le agrega una clase para que reciba estilos CSS
+  elementoLista.classList.add("elemento-lista")
+  //se agrega bajo el elemento selecionado de HTML
+  lista.appendChild(elementoLista)
 
 
-var botonBorrar = document.createElement("button")
-botonBorrar.textContent = "Eliminar invitado"
-botonBorrar.id = "boton-borrar"
-var corteLinea = document.createElement("br")
-elementoLista.appendChild(corteLinea)
-elementoLista.appendChild(botonBorrar);
+  //funcion para crear las cards de los invitados
+  function crearElemento(descripcion, valor) {
+    //se crean los elementos que contendran a los invitados
+    var spanNombre = document.createElement("span")
+    var inputNombre = document.createElement("span")
+    var espacio = document.createElement("br")
+    spanNombre.textContent = descripcion + ": "
+    inputNombre.innerHTML= valor; 
+    //se agregan las secciones de informacion de invitados bajo el elemento HTML selecionado
+    elementoLista.appendChild(spanNombre)
+    elementoLista.appendChild(inputNombre)
+    elementoLista.appendChild(espacio)
+  }
 
- botonBorrar.onclick = function() {
-// this.parentNode.style.display = 'none';
-botonBorrar.parentNode.remove()
+  crearElemento("Nombre", nombre)
+  crearElemento("Edad", edad)
+  crearElemento("Nacionalidad", nacionalidad)
+
+  //Boton que permite borrar a un invitado
+  var botonBorrar = document.createElement("button")
+  botonBorrar.textContent = "Eliminar invitado"
+  botonBorrar.id = "boton-borrar"
+  var corteLinea = document.createElement("br")
+  elementoLista.appendChild(corteLinea)
+  elementoLista.appendChild(botonBorrar);
+
+  botonBorrar.onclick = function() {
+    botonBorrar.parentNode.remove()
   }
 }
